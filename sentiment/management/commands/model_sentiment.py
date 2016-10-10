aa92b7e806b9478370ff4fc108287d06fb19cab7
mwunder <michael.wunder@gmail.com>
2016-10-09 20:23:27 -0400

    auto-scoring v1 (basic ensemble)

---
+++ sentiment/management/commands/model_sentiment.py
@@ -0,0 +1,12 @@
+from django.core.management.base import BaseCommand, CommandError
+from sentiment.models import *
+
+class Command(BaseCommand):
+    help = 'use the model in estimate_sentiment to estimate sentiment'
+
+    def add_arguments(self, parser):
+        parser.add_argument('poll_id', nargs='+', type=int)
+
+    def handle(self, *args, **options):
+        import estimate_sentiment 
+
