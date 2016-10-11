c382512d6680bde072882832165fe14d2ac5b156
Joe C. Antonelli <joecantonelli@gmail.com>
2016-10-10 20:16:27 -0700

    Add symbol selection functionality, refactor

--- twitter_stock/static/js/plotting.js
+++ twitter_stock/static/js/plotting.js
@@ -2,78 +2,78 @@
 
 $(function() {
 
-  function generateBins(values,symbols) {
-  
-      var bins = [];
+  //local var
+  var data = SCORES[sessionStorage.DATA_INDEX];
 
-      for (var i=0; i<values.length; i++) {
 
-          bins.push([0,0,0,0,0]);
+  (function addHistogram(callback) {
 
-          for (var j=0; j<values[i].length; j++) {
+      //create bins
+      var bins = [[0,0,0,0,0]];
+
+      for(var i=0; i<data.length; i++) { bins[0][Math.floor(2.49*(data[i]+1))] = bins[0][Math.floor(2.49*(data[i]+1))]+1 }
+
+      for(var j=0; j<5; j++) { bins[0][j] = bins[0][j]/(data.length) }
+
+      bins[0].unshift(sessionStorage.SYMBOL);
+
+
+      callback = function() {
+
+          //create chart
+          var chart = c3.generate({
       
-              var v = values[i][j]+1;
-      
-              bins[i][Math.floor(2.49*v)] = bins[i][Math.floor(2.49*v)]+1;
-          }
-    
-          for(var j=0; j<5; j++) { 
-      
-              bins[i][j] = bins[i][j]/(values[i].length);
-          }
-    
-          bins[i].unshift(symbols[i]);
-      }
+              data: {  
+                      columns: bins,
+                      type:    'bar'
+                    },
 
-      return bins;
-  }
+              bar:  {  width: { ratio: 0.9 }  }
+          });
+
+          //generate and load chart
+          $.ajax({
+
+              url:           'http://foundationphp.com/phpclinic/podata.php?&raw&callback=?',
+              jsonpCallback: 'jsonReturnData',
+              dataType:      'jsonp',
+              data:          { format: 'json' },
+
+              success: function(response) {
+          
+                  (function generateChart(data) {
+
+                      var chart = c3.generate({
+
+                          data: {  x: 'x',
+                                   xFormat: '%Y-%m-%d %H:%M:%S',
+                                   columns: data
+                                },
+
+                          axis: {
+                                   x: {  type: 'timeseries',
+
+                                         tick: {  format: '%m-%d %H:%M',
+                                                  culling: {  max: 5  }
+                                               }
+                                      }
+                                } 
+                      });
+                  }(processData(response)));
+              }
+          });
+      }();
+  }());
 
 
-  (function generateHist(binsObj) {
-  
-      var chart = c3.generate({
-      
-          data: {  
-                   columns: binsObj,
-                   type:    'bar'
-                },
+  //print symbol and scores info
+  [sessionStorage.SYMBOL, data].forEach(function(info) {
 
-          bar:  {  width: { ratio: 0.9 }  }
-      });
-  }( generateBins(scores,symbols) ));
+      var element = document.createElement('p');
 
+      element.appendChild(document.createTextNode(info));
+      element.style.textAlign = "center";
 
-  function loadChart() {
-
-      $.ajax({
-
-          url:           'http://foundationphp.com/phpclinic/podata.php?&raw&callback=?',
-          jsonpCallback: 'jsonReturnData',
-          dataType:      'jsonp',
-          data:          { format: 'json' },
-
-          success: function(response) {
-      
-              function generateChart(data) {
-  
-                  var chart = c3.generate({
-    
-                      data: {  x: 'x',
-                               xFormat: '%Y-%m-%d %H:%M:%S',
-                               columns: data
-                            },
-    
-                      axis: {
-                               x: {  type: 'timeseries',
-
-                                     tick: {  format: '%m-%d %H:%M',
-                                              culling: {  max: 5  }
-                                           }
-                                  }
-                            } 
-                  });
-              }(processData(response));
-          }
-      });
-  }
+      document.getElementById("output-text-container").appendChild(element);
+  });
 }); // Page Loaded
