1b60371006cc7890535b6e1ebf5b77974fb30416
Joe C. Antonelli <joecantonelli@gmail.com>
2016-10-12 15:12:29 -0700

    Fix symbol discrepancy bug in previous commit, add 'current_date' to the stock_sentiment() method dictionary

--- sentiment/views.py
+++ sentiment/views.py
@@ -13,9 +13,9 @@ class Score:
     ''' An object to represent all the scores of a stock on a specific day or interval
     '''
     def __init__(self,symbol,t,sc=0,f=0,z=0,c=0):
-        self.symbol=symbol
+        self.symbol = symbol
         self.created_at = t
-        self.scored_at= t 
+        self.scored_at = t 
         self.scores = sc
         self.freq = f
         self.z = z
@@ -25,21 +25,18 @@ class Score:
 def home(request):
     symbols = [s.symbol for s in Stock.objects.all()]
 
-    return render(request,'home.html', {
-               'symbols': map(str, symbols)
-           })
-
+    # Pass the symbols to page for use in the dropdown menu
+    return render(request,'home.html', { 'symbols': map(str, symbols) })
 
 def stock_sentiment(request):
     ''' The main function for displaying the sentiment scores for one or more stocks in the DB
     '''
 
     # Fetch the statuses from stock_status given the date constraints
-    interval = 1440 if 'w' not in request.GET or not is_num(request.GET['w']) else int(request.GET['w'])
+    interval     = 1440 if 'w' not in request.GET or not is_num(request.GET['w']) else int(request.GET['w'])
     current_date = datetime.strptime('2016-08-08','%Y-%m-%d') # Placeholder date, to be replaced or removed
-    end_date = get_date_from(request.GET,current_date) 
-    statuses = Stock_status.objects.filter( created_at__gte=end_date, 
-                created_at__lte=end_date+timedelta(minutes=interval))
+    end_date     = get_date_from(request.GET,current_date) 
+    statuses     = Stock_status.objects.filter(created_at__gte=end_date, created_at__lte=end_date+timedelta(minutes=interval))
     
     # Tally up all the sentiment scores from stock_status within valid range, organized by stock symbol
     symbol_scores = {}
@@ -53,6 +50,7 @@ def stock_sentiment(request):
 
     # Pass the raw sentiment scores to the page for presenting in visual form 
     return render(request,'stock_sentiment.html', {
+              'current_date':  current_date,
               'symbol_scores': set_scores(symbol_scores,end_date),
               'symbols':       map(str,symbol_scores.keys()), 
               'scores':        symbol_scores.values()
@@ -63,7 +61,6 @@ def stock_sentiment(request):
 def set_scores(sentiment,t):
     return [Score(sym,t,scores) for sym,scores in sentiment.items() ]
 
-
 def get_date_from(request,default_date=''):
     ''' Given a request for some date, parse it and return a valid date object.
         Valid forms, for August 9, 2016:
@@ -71,7 +68,7 @@ def get_date_from(request,default_date=''):
         "0809" (if called in 2016 only, otherwise will be August 9, XXXX where XXXX is current year)
     '''
     if 'date' not in request:
-        return  datetime.now()  if not default_date else default_date #id_to_datetime(datetime_id_day(datetime.now())) + timedelta(minutes=0)
+        return datetime.now() if not default_date else default_date
 
     try: 
         if len(request['date'])>4:
--- twitter_stock/static/js/plotting.js
+++ twitter_stock/static/js/plotting.js
@@ -1,79 +1,86 @@
 'use strict';
 
-$(function() {
+if(sessionStorage.DATA_INDEX) {
 
-  //local var
-  var data = SCORES[sessionStorage.DATA_INDEX];
+    $(function() {
 
+        //local var
+        var data = SCORES[sessionStorage.DATA_INDEX];
 
-  (function addHistogram(callback) {
+        (function addHistogram(callback) {
 
-      //create bins
-      var bins = [[0,0,0,0,0]];
+            //create bins
+            var bins = [ [0,0,0,0,0] ];
 
-      for(var i=0; i<data.length; i++) { bins[0][Math.floor(2.49*(data[i]+1))] = bins[0][Math.floor(2.49*(data[i]+1))]+1 }
+            for(var i=0; i<data.length; i++) { bins[0][Math.floor(2.49*(data[i]+1))] = bins[0][Math.floor(2.49*(data[i]+1))]+1 }
 
-      for(var j=0; j<5; j++) { bins[0][j] = bins[0][j]/(data.length) }
+            for(var j=0; j<5; j++) { bins[0][j] = bins[0][j]/(data.length) }
 
-      bins[0].unshift(sessionStorage.SYMBOL);
-
-
-      callback = function() {
-
-          //create chart
-          var chart = c3.generate({
-      
-              data: {  
-                      columns: bins,
-                      type:    'bar'
-                    },
-
-              bar:  {  width: { ratio: 0.9 }  }
-          });
-
-          //generate and load chart
-          $.ajax({
-
-              url:           'http://foundationphp.com/phpclinic/podata.php?&raw&callback=?',
-              jsonpCallback: 'jsonReturnData',
-              dataType:      'jsonp',
-              data:          { format: 'json' },
-
-              success: function(response) {
-          
-                  (function generateChart(data) {
-
-                      var chart = c3.generate({
-
-                          data: {  x: 'x',
-                                   xFormat: '%Y-%m-%d %H:%M:%S',
-                                   columns: data
-                                },
-
-                          axis: {
-                                   x: {  type: 'timeseries',
-
-                                         tick: {  format: '%m-%d %H:%M',
-                                                  culling: {  max: 5  }
-                                               }
-                                      }
-                                } 
-                      });
-                  }(processData(response)));
-              }
-          });
-      }();
-  }());
+            bins[0].unshift(sessionStorage.SYMBOL);
 
 
-  //print symbol and scores info
-  [sessionStorage.SYMBOL, data].forEach(function(info) {
+            callback = function() {
 
-      var element = document.createElement('p');
+                //create chart
+                var chart = c3.generate({
+            
+                    data: {  
+                            columns: bins,
+                            type:    'bar'
+                          },
 
-      element.appendChild(document.createTextNode(info));
-      element.style.textAlign = "center";
+                    bar:  {  width: { ratio: 0.9 }  }
+                });
 
-      document.getElementById("output-text-container").appendChild(element);
-  });
-}); // Page Loaded
+                //generate and load chart
+                $.ajax({
+
+                    url:           'http://foundationphp.com/phpclinic/podata.php?&raw&callback=?',
+                    jsonpCallback: 'jsonReturnData',
+                    dataType:      'jsonp',
+                    data:          { format: 'json' },
+
+                    success: function(response) {
+                
+                        (function generateChart(data) {
+
+                            var chart = c3.generate({
+
+                                data: {  x: 'x',
+                                         xFormat: '%Y-%m-%d %H:%M:%S',
+                                         columns: data
+                                      },
+
+                                axis: {
+                                         x: {  type: 'timeseries',
+
+                                               tick: {  format: '%m-%d %H:%M',
+                                                        culling: {  max: 5  }
+                                                     }
+                                            }
+                                      } 
+                            });
+                        }(processData(response)));
+                    }
+                });
+            }();
+        }());
+
+
+        //print symbol and scores info to the page, clear sessionStorage
+        [sessionStorage.SYMBOL, data].forEach(function(info, callback) {
+
+            var element = document.createElement('p');
+
+            element.appendChild(document.createTextNode(info));
+            element.style.textAlign = "center";
+            document.getElementById("output-text-container").appendChild(element);
+
+            callback = function() {
+
+                sessionStorage.removeItem('SYMBOL');
+                sessionStorage.removeItem('DATA_INDEX');
+            }();
+        });
+    });
+}
