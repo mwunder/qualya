4a04f83437219cf41a47731a063b92b0b45e6e95
Joe C. Antonelli <joecantonelli@gmail.com>
2016-10-27 18:05:23 -0700

    Add 'dev_notes.txt' to share comments/issues, remove 'plotting.js', add 'chartist.js' and 'chartist.css', add 'global.css' to manage repeated styles, add a debug mode and stock sentiment graphics to the 'stock_sentiment_universe' view

--- sentiment/views.py
+++ sentiment/views.py
@@ -37,7 +37,7 @@ def stock_sentiment_universe(request):
 
     # Fetch the statuses from stock_status given the date constraints
     interval     = 1440 if 'w' not in request.GET or not is_num(request.GET['w']) else int(request.GET['w'])
-    current_date = datetime.strptime('2016-08-08','%Y-%m-%d') # Placeholder date, to be replaced or removed
+    current_date = datetime.now() # datetime.strptime('2016-08-08','%Y-%m-%d') <-- placeholder date
     end_date     = get_date_from(request.GET,current_date)
     statuses     = Stock_status.objects.filter(created_at__gte=end_date, created_at__lte=end_date+timedelta(minutes=interval))
 
@@ -54,37 +54,40 @@ def stock_sentiment_universe(request):
 
     # Pass the raw sentiment scores to the page for presenting in visual form 
     return render(request,'stock_sentiment_universe.html', {
-              'date':          end_date,
-              'symbols':       map(str, symbol_scores.keys()),
-              'scores':        symbol_scores.values(),
-              'symbol_scores': set_scores(symbol_scores, end_date)
-            })
+               'date':          end_date,
+               'symbols':       map(str, symbol_scores.keys()),
+               'scores':        symbol_scores.values(),
+               'symbol_scores': set_scores(symbol_scores, end_date)
+           })
 
 
 def stock_sentiment_historical(request):
     ''' The main function for displaying the sentiment scores for a single stock over time in the DB
     '''
 
-    if 'symbol' not in request.GET:
-        return HttpResponse("<html><body>'No stock symbol found'</body></html>") 
     symbol = request.GET['symbol']
+
+    if 'symbol' not in request.GET:
+        return HttpResponse("<html><body>'No stock symbol found'</body></html>")
 
     # Fetch the statuses from stock_status given the date constraints
     interval     = 1440*7 if 'w' not in request.GET or not is_num(request.GET['w']) else int(request.GET['w'])
-    current_date = datetime.now() # datetime.strptime('2016-08-08','%Y-%m-%d') # Placeholder date, to be replaced or removed
-    
-    end_date     = get_date_from(request.GET,current_date) 
+    current_date = datetime.now() # datetime.strptime('2016-08-08','%Y-%m-%d') <-- placeholder date
+    end_date     = get_date_from(request.GET,current_date)
     stock        = Stock.objects.filter(symbol=symbol.lower())
     statuses     = Stock_status.objects.filter(stock=stock,created_at__gte=end_date-timedelta(minutes=interval), created_at__lte=end_date)
-    prices       = Stock_price.objects.filter(stock=stock,trading_day__gte=end_date-timedelta(minutes=interval), trading_day__lte=end_date)
+
+    # Fetch stock prices
+    prices = Stock_price.objects.filter(stock=stock,trading_day__gte=end_date-timedelta(minutes=interval), trading_day__lte=end_date)
 
     # Tally up all the sentiment scores from stock_status within valid range, organized by stock symbol
     stock_sentiment_history = {}
-    bins = defaultdict(list)
-    closes = dict([(datetime.date(p.trading_day),p.close_price) for p in prices])  
 
-    for status in statuses: 
-        if not  status.status_sentiment or status.status_sentiment < -1 or status.status_sentiment > 1: continue 
+    bins   = defaultdict(list)
+    closes = dict([(datetime.date(p.trading_day),p.close_price) for p in prices])
+
+    for status in statuses:
+        if not  status.status_sentiment or status.status_sentiment < -1 or status.status_sentiment > 1: continue
         try:    stock_sentiment_history[datetime.date(status.created_at)].append(status.status_sentiment)
         except: stock_sentiment_history[datetime.date(status.created_at)] = [status.status_sentiment]
         bins[datetime.date(status.created_at)].append(status.sentiment_bin)
@@ -92,47 +95,52 @@ def stock_sentiment_historical(request):
     for day,history in stock_sentiment_history.items():
         stock_sentiment_history[day] = sorted(history)
         bins[day] = map(lambda x:x-1,zip(* sorted(Counter(bins[day]+[-2,-1,0,1,2]).most_common(5)))[1])
-        if day not in closes: 
+        if not closes: continue
+        if day not in closes:
             for d,close in sorted(closes.items()):
                 if d<day: closes[day] = close
-        if day not in closes: closes[day] =  sum(closes.values())/len(closes)
+        if day not in closes: closes[day] = sum(closes.values())/len(closes)
 
-    dates,scores_by_date = zip(* sorted(stock_sentiment_history.items()))
-    dates,bins = zip(* sorted(bins.items()))
-    if closes: _,closes = zip(* sorted(closes.items())) 
-    else:        closes = [0]*len(dates)
+    for day in closes.keys():
+        if day not in stock_sentiment_history: del closes[day]
+
+    dates, scores_by_date = zip(* sorted(stock_sentiment_history.items()))
+    dates, bins           = zip(* sorted(bins.items()))
+
+    if closes: _, closes  = zip(* sorted(closes.items()))
+    else:         closes  = [0]*len(dates)
 
     dates = map(int,map(lambda d: d.day,dates))
-    bins , scores_by_date = list(bins), list(scores_by_date)
+    bins, scores_by_date = list(bins), list(scores_by_date)
 
     print bins
+
     # Pass the raw sentiment scores to the page for presenting in visual form 
     return render(request,'stock_sentiment_historical.html', {
-              'current_stock':  symbol,
-              'dates':          dates,
-              'closes':         closes,
-              'scores_by_date': scores_by_date,
-              'bins':           bins
+               'current_stock':  symbol,
+               'dates':          dates,
+               'closes':         list(closes),
+               'scores_by_date': scores_by_date,
+               'bins':           bins
            })
 
 
 #HELPERS
 def get_date_from(request,default_date=''):
-    ''' Given a request for some date, parse it and return a valid date object.
-        Valid forms, for August 9, 2016:
-        "2016-08-09"
-        "0809" (if called in 2016 only, otherwise will be August 9, XXXX where XXXX is current year)
+    ''' 
+    Given a request for some date, parse it and return a valid date object. As an example, valid forms for August 9, 2016 are:
+        (1) "2016-08-09"
+        (2) "0809"      (if called in 2016 only, otherwise will be August 9, XXXX where XXXX is current year)
     '''
+
     if 'date' not in request:
         return datetime.now() if not default_date else default_date
-
     try: 
         if len(request['date'])>4:
             return datetime.strptime(request['date'],'%Y-%m-%d')
         else:
-            return id_to_datetime(str(datetime.now().year)+
-                request['date'].replace('/','')+'000000')
-    except: 
+            return id_to_datetime(str(datetime.now().year)+request['date'].replace('/','')+'000000')
+    except:
         return datetime.now() if not default_date else default_date
 
     return datetime.now() if not default_date else default_date
@@ -140,4 +148,3 @@ def get_date_from(request,default_date=''):
 
 def set_scores(sentiment,t):
     return [Score(sym,t,scores) for sym,scores in sentiment.items() ]
-
--- twitter_stock/static/js/stock_sentiment_universe.js
+++ twitter_stock/static/js/stock_sentiment_universe.js
@@ -1,5 +1,10 @@
 'use strict';
 
+var DEBUG_UNIVERSE = true;
+
+
+/* METHODS ========================================================================================================================================*/
+
 var printUniverseInfo = function() {
 
     //print info about the stock universe to the page
@@ -9,33 +14,158 @@ var printUniverseInfo = function() {
 
     if(SYMBOLS.length>0) {
 
-        universe.appendChild(document.createTextNode("On "+DATE+", the Universe contains "+SYMBOLS.length+" stock(s)."));
+        //pluralize string if necessary
+        var stk = SYMBOLS.length == 1 ? ' stock' : ' stocks';
+
+        universe.appendChild(document.createTextNode("On "+DATE+", the Universe contains "+SYMBOLS.length+stk+"."));
 
     } else {
 
-        universe.appendChild(document.createTextNode("Sorry, but there are no stocks in the Universe on "+DATE+"."));
+        universe.appendChild(document.createTextNode("Sorry, there are no stocks in the Universe on this date."));
     }
 
     document.getElementById("universe-container").appendChild(universe);
 }
 
 
-var addSentimentBars = function() {
+var addSentimentGraphics = function() {
+
+    //local vars
+    var className = 'sentiment-bar',
+        width     = 500,
+        height    = 50,
+        cols      = ['rgba(255,0,0,0.9)', 'rgba(255,128,0,0.9)', 'rgba(128,128,128,1)', 'rgba(255,255,0,0.9)', 'rgba(0,255,0,0.9)'],
+        bordRad   = '5px';
+
+
+    var filterBins = function(num) {
+
+        var binIndicies = [];
+
+        for(var n=0; n<BINS[num].length; n++) { if(BINS[num][n] != 0) { binIndicies.push(n) } }
+
+        return binIndicies;
+    }
+
 
     //add sentiment bars to the page
     for(var i=0; i<SYMBOLS.length; i++) {
 
-        var s_bar   = document.createElement("canvas"),
-            context = s_bar.getContext('2d'); 
+        //loop vars
+        var wrapper    = document.createElement("div"),
+            s_bar      = document.createElement("canvas"),
+            context    = s_bar.getContext('2d'),
+            gradient   = context.createLinearGradient(0, 0, width, 0),
+            indicies   = filterBins(i),
+            colStopTot = 0,
+            style      = s_bar.style;
 
-        s_bar.className = "sentiment-bar";
-        s_bar.width     = 500;
-        s_bar.height    = 50;
+        //attributes
+        s_bar.id        = SYMBOLS[i];
+        s_bar.className = className;
+        s_bar.width     = width;
+        s_bar.height    = height;
+        s_bar.onclick   = (function(i) { return function() { location.href = "/stock_sentiment_historical/?symbol="+SYMBOLS[i] } }(i));
 
-        context.fillStyle = "red";
-        context.fillRect(0, 0, s_bar.width, s_bar.height);
+        //add color stops to the gradient
+        for(var j=0; j<indicies.length; j++) {
 
-        document.getElementById("sentiment-bar-container").appendChild(s_bar);
+            //start color
+            gradient.addColorStop(colStopTot, cols[indicies[j]]);
+
+            //update
+            colStopTot += BINS[i][indicies[j]];
+
+            //stop color
+            gradient.addColorStop(colStopTot, cols[indicies[j]]);
+        }
+
+        //gradient attribute
+        context.fillStyle = gradient;
+
+        //apply gradient to the canvas
+        context.fillRect(0, 0, width, height);
+
+        //symbol text attributes
+        context.fillStyle    = '#b3d9ff';
+        context.font         = '20px Helvetica';
+        context.textAlign    = 'center';
+        context.textBaseline = 'middle';
+
+        //write text to the canvas 
+        context.fillText(SYMBOLS[i], width/2, height/2);
+
+        //add border radius styles if the current bar is the first or last
+        if(i == 0) {
+
+            style.borderTopLeftRadius  = bordRad;
+            style.borderTopRightRadius = bordRad;
+
+        } else if(i == SYMBOLS.length-1) {
+
+            style.borderBottomLeftRadius  = bordRad;
+            style.borderBottomRightRadius = bordRad;
+        }
+
+        wrapper.appendChild(s_bar);
+        document.getElementById("sentiment-bars-container").appendChild(wrapper);
     }
+}
+
+
+/* DEBUG METHODS ==================================================================================================================================*/
+
+var generateBins = function(callback) {
+    
+    //create bins for the chart from DB data
+    for(var i=0; i<SCORES.length; i++) {
+
+        BINS.push([0,0,0,0,0]);
+
+        for (var j=0; j<SCORES[i].length; j++) {
+
+            var s = SCORES[i][j]+1;
+
+            BINS[i][Math.floor(2.49*s)] = BINS[i][Math.floor(2.49*s)]+1;
+        }
+
+        for(var j=0; j<5; j++) { BINS[i][j] = BINS[i][j]/(SCORES[i].length) }
+    }
+
+    if(typeof callback === 'function') { callback() }
+}
+
+
+var addHistogram = function() {
+
+    //add 'chartist.js' chart to the page
+    var chartData = {
+
+            labels: ['0', '1', '2', '3', '4'],
+
+            series: BINS
+        },
+
+        chartOptions = {
+
+            width: '1400px',
+
+            height: '700px',
+
+            high: 1,
+
+            low: 0
+        };
+
+    new Chartist.Bar('.ct-chart', chartData, chartOptions);
+}
+
+
+var toggleHistogram = function() {
+
+    //hide or unhide the histogram
+    var histCont = document.getElementById("histogram-container").style;
+
+    if(histCont.display == 'block') { histCont.display = 'none' } else { histCont.display = 'block' }
 }
 
