5137d5e501338c3b498800a6028b7bf109650b99
mwunder <michael.wunder@gmail.com>
2016-10-30 17:49:48 -0400

    Combining GOOG and GOOGL for concision

--- sentiment/views.py
+++ sentiment/views.py
@@ -53,7 +53,7 @@ def stock_sentiment_universe(request):
     
     for status in statuses: 
         if not  status.status_sentiment or status.status_sentiment < -1 or status.status_sentiment > 1: continue 
-        sym = status.symbol if status.symbol.lower()!='GOOG' else 'GOOGL'
+        sym = status.symbol if status.symbol.lower()!='goog' else 'GOOGL'
         try:    symbol_scores[sym].append(status.status_sentiment)
         except: symbol_scores[sym] = [status.status_sentiment]
         bins[sym].append(status.sentiment_bin)
