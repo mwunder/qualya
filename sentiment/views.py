834daa437cd45d8d6599fbc0659fcf4f1d3d8966
mwunder <michael.wunder@gmail.com>
2016-11-05 00:48:51 -0400

    Shifting the current date back one day, since the estimates wont update until the end of day

--- sentiment/views.py
+++ sentiment/views.py
@@ -37,10 +37,10 @@ def stock_sentiment_universe(request):
 
     # Fetch the statuses from stock_status given the date constraints
     interval     = 1440 if 'w' not in request.GET or not is_num(request.GET['w']) else int(request.GET['w'])
-    current_date = datetime.now() # datetime.strptime('2016-08-08','%Y-%m-%d') <-- placeholder date
+    current_date = datetime.now()-timedelta(minutes=interval) # datetime.strptime('2016-08-08','%Y-%m-%d') <-- placeholder date
     end_date     = get_date_from(request.GET,current_date)
     end_date     = datetime(end_date.year,end_date.month,end_date.day)
-    statuses     = Stock_status.objects.filter(created_at__gte=end_date-timedelta(minutes=interval), created_at__lte=end_date)
+    statuses     = Stock_status.objects.filter(created_at__gte=end_date, created_at__lte=end_date+timedelta(minutes=interval))
     prices       = Stock_price.objects.filter(trading_day__gt=end_date-timedelta(minutes=interval+1400), trading_day__lte=end_date)
     stocks       = Stock.objects.all()
 
@@ -95,7 +95,7 @@ def stock_sentiment_historical(request):
     end_date     = get_date_from(request.GET,current_date)
     end_date     = datetime(end_date.year,end_date.month,end_date.day)
     stock        = Stock.objects.filter(symbol=symbol.lower())
-    statuses     = Stock_status.objects.filter(stock=stock,created_at__gte=end_date-timedelta(minutes=interval), created_at__lte=end_date)
+    statuses     = Stock_status.objects.filter(stock=stock,created_at__gte=end_date-timedelta(minutes=interval-1440), created_at__lte=end_date+timedelta(minutes=1440))
 
     # Fetch prices
     prices = Stock_price.objects.filter(stock=stock,trading_day__gte=end_date-timedelta(minutes=interval+1440*3), trading_day__lte=end_date)
