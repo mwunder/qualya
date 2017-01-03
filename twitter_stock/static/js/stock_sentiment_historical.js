b829970c1ed29434f2f46702c87ddeec91402550
Joe C. Antonelli <joecantonelli@gmail.com>
2017-01-02 19:09:08 -0500

    Historical sub-nav layout

--- twitter_stock/static/js/stock_sentiment_historical.js
+++ twitter_stock/static/js/stock_sentiment_historical.js
@@ -26,3 +26,40 @@ var addGoButtonClickEvent = function() {
         location.href = "/stock_sentiment_historical/?symbol="+selected+"&w="+timeFrame+"&date="+DATE;
     }
 }
+
+var addLineGraph = function() {
+
+    //add 'chartist.js' chart to the page
+    var chartData = {
+
+            labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
+
+            series: [ [12, 9, 7, 8, 5], [2, 1, 3.5, 7, 3], [1, 3, 4, 5, 6] ]
+        },
+
+        chartOptions = {
+
+            width: 640,
+
+            height: 480,
+
+            axisX: {
+
+
+            },
+
+            axisY: {
+
+
+            },
+
+            showPoint: false,
+
+            lineSmooth: Chartist.Interpolation.monotoneCubic()
+        };
+
+    var chart = new Chartist.Line('.ct-chart', chartData, chartOptions);
+
+    //remove gridlines
+    chart.on('draw', function(data) { if(data.type === 'grid' && data.index !== 0) { data.element.remove() } });
+}
