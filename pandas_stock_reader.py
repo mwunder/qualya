2eb2a6853da8b050cd328640766d83a9bf5983f9
mwunder <michael.wunder@gmail.com>
2017-12-23 17:43:32 -0500

    Short term fix for price feed.

--- pandas_stock_reader.py
+++ pandas_stock_reader.py
@@ -2,6 +2,9 @@ from datetime import *
 from sentiment.models import *
 from sentiment_refs import * 
 
+import json
+import urllib2
+
 try: 
     import pandas.io.data as web
 except:
@@ -14,13 +17,43 @@ end = datetime.now() #(2016,10,7)
 
 toupper = lambda s: s.upper()
 
-try:
-    stockDataYear = web.DataReader(map(toupper,symbols), 'google', start, end).to_frame()
-except:
-    stockDataYear = web.DataReader(map(toupper,symbols), 'yahoo', start, end).to_frame()
+# try:
+#     stockDataYear = web.DataReader(map(toupper,symbols), 'google', start, end).to_frame()
+# except:
+#     stockDataYear = web.DataReader(map(toupper,symbols), 'yahoo', start, end).to_frame()
+
+API_KEY = '599JJSD3VV041YHV'
+URL_ACCESS = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={}&apikey=599JJSD3VV041YHV'
+
+
+def pull_stock_data(symbol):
+    response = urllib2.urlopen(URL_ACCESS.format(symbol))
+    price_data = json.loads(response.read())["Time Series (Daily)"]
+    prices = pd.DataFrame()
+    for d, data in price_data.items():
+        data['date'] = pd.to_datetime(d)
+        for k, v in data.items():
+            data[k] = [v]
+        prices = pd.concat([prices, pd.DataFrame(data)])
+
+    prices.columns = ['Open', 'High', 'Low', 'Close', 'adj', 'Volume', 'div', 'split', 'date']
+    prices['symbol'] = symbol
+    return prices
+
+stockDataYear = pd.DataFrame()
+
+for symbol in map(toupper,symbols):
+    try:
+        stockDataYear = pd.concat([stockDataYear, pull_stock_data(symbol)])
+    except:
+        print ('symbol not found: ', symbol)
+        continue 
 
 for i,row in stockDataYear.iterrows():
-    day,symbol = row.name
+    # day,symbol = row.name
+    day = row['date'].to_datetime()
+    symbol = row['symbol']
+
     stock = Stock.objects.filter(symbol=symbol)
     if not stock: continue
     stock = stock[0]
